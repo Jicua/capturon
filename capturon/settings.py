ace3cd4e763d4eec2498408af1345b4a7b61ff51
Javier <jquappev@gmail.com>
2020-03-03 13:45:01 -0300

    arreglo static files

--- capturon/settings.py
+++ capturon/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn_test')
+STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+
+MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
+MEDIA_URL = '/media/'
--- capturon/urls.py
+++ capturon/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import home_page, contacto_page
-from asistencia.views import justificacion_detail_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,10 @@ urlpatterns = [
     path('', home_page),
     path('contacto/', contacto_page)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
